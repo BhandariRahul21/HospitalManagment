3729e838ad1d93d570e2db527f7999b8da9d927e
BhandariRahul21 <86076899+BhandariRahul21@users.noreply.github.com>
2022-02-24 19:36:16 +0530

    hello

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+let app = express()
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect('mongodb+srv://todoAppUser:qwertyuiop@cluster0.yuair.mongodb.net/Hospital_Mangment?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const notesSchema ={
+    name:String,
+    email:String,
+    subject:String,
+    message:String
+}
+const Note= mongoose.model("Note",notesSchema)
+
+
+
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+
+
+
+app.post('/create-item',function(req,res){
+    // console.log(req.body.email)
+    let newBlog = new Note({
+        name:req.body.name,
+        email:req.body.email,
+        
+        message:req.body.message 
+    })
+    // console.log(newNote)
+    newBlog.save();
+    // res.send("Thanks For Submiting the form");
+    res.redirect('/contact.html')
+})
+app.listen(3000,function(){
+    console.log("server is running on 3000")
+})
+
